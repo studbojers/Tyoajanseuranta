1bc462aee39084d78159e95851cb346b521863e8
studbojers <sdebastian.bojers@stud.pointcollege.fi>
2016-10-16 22:48:35 +0300

    Added the sw project for the project.

---
+++ Tyoajanseuranta/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tyoajanseuranta.Startup))]
+namespace Tyoajanseuranta
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
